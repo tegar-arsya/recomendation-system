2a211af667783a0b13131e8d25a21d8e13f5c1cd
tegar-arsya <tegararsya0117@gmail.com>
2025-03-14 21:22:14 +0700

    feat:

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
+import createError from 'http-errors';
 import express from 'express';
 import cookieParser from 'cookie-parser';
 import logger from 'morgan';
 import firebase from './config/firebase.js';
-import { specs, swagerUi } from './config/swager.js';
-import dotenv from 'dotenv';
+import {specs, swagerUi} from './config/swager.js';
+import dotenv from 'dotenv'; // Menggunakan dotenv untuk memuat variabel lingkungan
 
 // routes
 import indexRouter from './routes/index.js';
@@ -13,7 +14,7 @@ dotenv.config();
 
 firebase();
 
-const app = express();
+var app = express();
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -27,13 +28,15 @@ app.use('/seeder', seederRouter);
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
-    res.status(404).json({ error: "Not Found" });
+    next(createError(404));
 });
 
 // error handler
 app.use((err, req, res, next) => {
-    res.status(err.status || 500).json({ error: err.message });
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.status(err.status || 500);
+  res.render('error');
 });
 
-// ✅ Ekspor sebagai handler agar bisa dijalankan di Vercel
-export default app;
+export default app; 
