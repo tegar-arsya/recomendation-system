9a237eb89d8015d359c608f9ce535f901500f48e
Dewa Putra Hernanda <dewananda089@gmail.com>
2025-03-16 03:03:03 +0700

    fix deploy

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ import {swagerUi} from './config/swager.js';
 import dotenv from 'dotenv'; // Menggunakan dotenv untuk memuat variabel lingkungan
 import fs from 'fs'
 import yaml from 'yaml'
+import http from 'http';
 
 // routes
 import indexRouter from './routes/index.js';
@@ -28,7 +29,7 @@ app.use(cookieParser());
 
 app.use('/api-docs', swagerUi.serve, swagerUi.setup(swagerDocument));
 
-app.use('/', indexRouter);
+app.use('/', swagerUi.serve, swagerUi.setup(swagerDocument));
 app.use('/seeder', seederRouter);
 app.use('/recomendation_system', recomendationSyestemRouter);
 
@@ -44,5 +45,48 @@ app.use((err, req, res, next) => {
   res.status(err.status || 500);
   res.render('error');
 });
+
+// Setup server
+const port = normalizePort(process.env.PORT || '3000');
+app.set('port', port);
+
+const server = http.createServer(app);
+
+server.listen(port, () => {
+    console.log(`Server running on http://localhost:${port}`);
+});
+
+server.on('error', onError);
+server.on('listening', onListening);
+
+function normalizePort(val) {
+    const port = parseInt(val, 10);
+    if (isNaN(port)) return val;
+    if (port >= 0) return port;
+    return false;
+}
+
+function onError(error) {
+    if (error.syscall !== 'listen') throw error;
+    const bind = typeof port === 'string' ? 'Pipe ' + port : 'Port ' + port;
+    switch (error.code) {
+        case 'EACCES':
+            console.error(`${bind} requires elevated privileges`);
+            process.exit(1);
+            break;
+        case 'EADDRINUSE':
+            console.error(`${bind} is already in use`);
+            process.exit(1);
+            break;
+        default:
+            throw error;
+    }
+}
+
+function onListening() {
+    const addr = server.address();
+    const bind = typeof addr === 'string' ? 'pipe ' + addr : 'port ' + addr.port;
+    console.log(`Listening on ${bind}`);
+}
 
 export default app; 
