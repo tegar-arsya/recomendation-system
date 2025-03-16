4d1f12a3306a84b306c20e92346e48bb431a2400
tegar-arsya <tegararsya0117@gmail.com>
2025-03-16 14:09:07 +0700

    perbaiki utils

--- config/utils.js
+++ config/utils.js
@@ -3,12 +3,21 @@ import path from 'path';
 import { fileURLToPath } from 'url';
 
 export default function utils() {
-    const loadJSON = async (relativePath) => {
+    const loadJSON = async () => {
         try {
+            // Dapatkan path dari environment variable
+            const relativePath = process.env.DATABASE_PATH;
+            if (!relativePath) {
+                throw new Error('Environment variable DATABASE_PATH tidak ditemukan.');
+            }
+
             // Dapatkan path absolut ke file JSON
             const __filename = fileURLToPath(import.meta.url);
             const __dirname = path.dirname(__filename);
             const jsonFilePath = path.resolve(__dirname, relativePath);
+
+            // Debug path
+            console.log('Mencoba membaca file JSON dari path:', jsonFilePath);
 
             // Baca file JSON
             const data = await readFile(jsonFilePath, 'utf-8');
@@ -19,7 +28,7 @@ export default function utils() {
             }
             return parsedData;
         } catch (error) {
-            console.error(`❌ Error loading JSON file: ${relativePath}`, error.message);
+            console.error(`❌ Error loading JSON file:`, error.message);
             return null;
         }
     };
